5fcbeba5aac6222d648a1d8155b49398bd947baf
Lars Grefer <github@larsgrefer.de>
2019-11-19 22:51:38 +0100

    Use io.freefair.android.aspectj.transform

--- Hero/ritz/src/main/java/com/example/ritz/TheWeaver.java
+++ Hero/ritz/src/main/java/com/example/ritz/TheWeaver.java
@@ -1,8 +1,8 @@
 package com.example.ritz;
 
 import android.util.Log;
+
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -10,10 +10,9 @@ import org.aspectj.lang.annotation.Before;
 public class TheWeaver {
 
     @Before("execution(* com.example.ritz.Cheese.sayChop(..))")
-    public void bulbs(ProceedingJoinPoint joinPoint) throws Throwable {
+    public void bulbs(JoinPoint joinPoint) throws Throwable {
         System.out.println("Please work");
         Log.w("Lightbulb","We successfully weaved");
-        joinPoint.proceed();
     }
 
 
